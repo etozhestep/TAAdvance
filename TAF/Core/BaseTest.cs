d8727bfffa692eb47b52d7e31c6819340c46526a
Andrei Sciapaniuk <Andrei.Sciapaniuk@coxautoinc.com>
2024-11-25 18:29:27 +0400

    implement usings in BaseTest

--- TAF/Core/BaseTest.cs
+++ TAF/Core/BaseTest.cs
@@ -1,3 +1,11 @@
+using System.Reflection;
+using Allure.Net.Commons;
+using Allure.NUnit.Attributes;
+using NLog;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using TAF.Core.Util;
+
 namespace TAF.Core;
 
 public class BaseTest
